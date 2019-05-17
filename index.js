8a3c935b8b7ec44f3b065202fcf70278a428e448
ivan.sampedro@oracle.com <ivan.sampedro@oracle.com>
2019-05-17 17:01:03 +0000

    Update index.js

--- index.js
+++ index.js
@@ -115,10 +115,10 @@ function postToStream(codestring) {
           ]
   });
 
-  //https://soa.wedoteam.io',
+  //https://soa.wedoteam.io', https://130.61.94.91, streams
   // An object of options to indicate where to post to
   var post_options = {
-      host: 'https://130.61.94.91',
+      host: 'streams',
       port: '443',
       path: '/wedodevops/publish/madrid/devops',
       method: 'POST',
